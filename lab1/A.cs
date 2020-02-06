5530d362d41e4e729037781f99d2dda30f377383
Swagrosemedusa <voronin.voronin-maikl@yandex.ru>
2020-02-06 13:10:09 +0300

    lab1

---
+++ lab1/A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class A
+    {
+
+    }
+    class B
+    {
+        public int a;
+        public int b;
+    }
+    class C:B
+    {
+
+    }
+    
+}
